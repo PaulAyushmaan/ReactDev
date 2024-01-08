e2739e381781c0609a2aba1db84e949db9c82f34
PaulAyushmaan <ayushmaanpaul2004@gmail.com>
2024-01-08 11:36:25 +0530

    Chapter 2 updated

---
+++ 2_Igniting_Our_App/App.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+/// https://parceljs.org/
+//https://browserslist.dev/?q=bGFzdCAyIHZlcnNpb24%3D
+/*
+<div id="paremt">
+			<div id="child1">
+				<h1>I am h1 Tag</h1>
+				<h2>I am h2 Tag</h2>
+			</div>
+            <div id="child2">
+				<h1>I am h1 Tag</h1>
+				<h2>I am h2 Tag</h2>
+			</div>
+		</div>
+        ReactElement(Object)=>HTML(Browser Understands) 
+*/
+// For creating this nesting element
+const parent = React.createElement('div', { id: 'parent' }, [
+	React.createElement('div', { id: 'child1' }, [
+		React.createElement('h1', {}, 'I am h1 Tag'),
+		React.createElement('h2', {}, 'This is h2 Tag'),
+	]),
+	React.createElement('div', { id: 'child2' }, [
+		React.createElement('h1', {}, 'I am h1 Tag'),
+		React.createElement('h2', {}, 'I am h2 Tag'),
+	]),
+]);
+const header = React.createElement(
+	//header is a react element and react element is a normal JS Object
+	'h1',
+	{ id: 'heading', xyz: 'abc' }, //props
+	'Hello World From React', //props
+);
+//console.log(header);
+const root = ReactDOM.createRoot(document.querySelector('#root'));
+//console.log(root);
+//root.render(header);
+root.render(parent);
